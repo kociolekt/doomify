7f2085cd7ef6643c3376e487bea9259069010442
Tomasz Kociołek <tomasz.kociolek@k2.pl>
2016-02-09 14:13:27 +0100

    splitting into different files

--- index.js
+++ index.js
@@ -19,89 +19,19 @@ var DEFAULTS = {
 
 function compile(str, minifierOpts) {
   var minified = minifierOpts === false ? str : minify(str, minifierOpts);
+  Mustache.parse(minified);
   return minified;
 }
 
 function wrap(source) {
-  return 'var doomify = require(\'doomify\');\n' +
-    'module.exports = doomify.template(\'' + source + '\');';
+  return 'var doomifyProcess = require(\'doomify/process\');\n' +
+    'module.exports = doomifyProcess(\'' + source + '\');';
 }
 
 function transform(src, opts) {
   var compiled = compile(src, opts.noMinify ? false : opts.minifierOpts);
   var body = wrap(compiled);
   return body;
-}
-
-// ie 9, ie 10
-if (
-  (typeof Range !== 'undefined') &&
-  !Range.prototype.createContextualFragment
-) {
-  Range.prototype.createContextualFragment = function(html) {
-    var
-      frag = document.createDocumentFragment(),
-      div = document.createElement('div');
-    frag.appendChild(div);
-    div.outerHTML = html;
-    return frag;
-  };
-}
-
-// without additional div
-function getDocumentFragmentFromString(html) {
-  // return document.createRange().createContextualFragment(html); // FU safari
-  var range = document.createRange();
-  range.selectNode(document.body); // safari
-  return range.createContextualFragment(html);
-}
-
-function getElementsFromDom(dom, type, name) {
-
-  var elems;
-
-  if (!name) throw new Error('no-name not allowed');
-
-  if (typeof dom === 'string') {
-    dom = getDocumentFragmentFromString(dom);
-  }
-
-  // check also for getDocumentFragmentFromString
-  if (!dom.nodeType) {
-    throw new Error('string or DOM node');
-  }
-
-  // nodes of interest :)
-  dom.noi = {};
-
-  if (type === 'data') name = 'data-' + name;
-
-  if (type === 'data' || type === 'attr') {
-    elems = dom.querySelectorAll('[' + name + ']');
-  } else if (type === 'class') {
-    elems = dom.querySelectorAll('[class*="' + name + '-"]');
-  } else {
-    throw new Error('type "' + type + '" unsupported');
-  }
-
-  Array.from(elems).forEach(function(e){
-
-    var letName;
-
-    if (type === 'data' || type === 'attr') {
-      letName = e.getAttribute(name);
-    } else {
-      letName = e.className.match(new RegExp(name + '-' + '((-?\\w+)+)'));
-      if (letName && letName.length) letName = letName[1];
-    }
-
-    if (!letName) return;
-    if (!dom.noi[letName]) dom.noi[letName] = [];
-    if (dom.noi[letName]) dom.noi[letName].push(e);
-
-  });
-
-  return dom;
 }
 
 var templateExtension = /\.(dmt|tpl|html)$/;
@@ -143,23 +73,7 @@ function doomify(file, opts) {
   return new Doomify(opts);
 }
 
-function contextualFragmentToTemplate(fragment) {
-  var retval = fragment.children;
-  return objectAssign(retval, fragment.noi);
-}
-
-function template(source) {
-  var cachedTemplate = Mustache.parse(source);
-  return function(data, partials) {
-    return contextualFragmentToTemplate(getElementsFromDom(
-      Mustache.render(source, data, partials),
-      'attr',
-      'data-doomify'));
-  };
-}
-
 module.exports = doomify;
 module.exports.compile = compile;
 module.exports.wrap = wrap;
 module.exports.transform = transform;
-module.exports.template = template;
---
+++ process/index.js
@@ -0,0 +1,89 @@
+var Mustache = require('mustache');
+var objectAssign = require('object-assign');
+
+// ie 9, ie 10
+if (
+  (typeof Range !== 'undefined') &&
+  !Range.prototype.createContextualFragment
+) {
+  Range.prototype.createContextualFragment = function(html) {
+    var
+      frag = document.createDocumentFragment(),
+      div = document.createElement('div');
+    frag.appendChild(div);
+    div.outerHTML = html;
+    return frag;
+  };
+}
+
+// without additional div
+function getDocumentFragmentFromString(html) {
+  // return document.createRange().createContextualFragment(html); // FU safari
+  var range = document.createRange();
+  range.selectNode(document.body); // safari
+  return range.createContextualFragment(html);
+}
+
+function getElementsFromDom(dom, type, name) {
+
+  var elems;
+
+  if (!name) throw new Error('no-name not allowed');
+
+  if (typeof dom === 'string') {
+    dom = getDocumentFragmentFromString(dom);
+  }
+
+  // check also for getDocumentFragmentFromString
+  if (!dom.nodeType) {
+    throw new Error('string or DOM node');
+  }
+
+  // nodes of interest :)
+  dom.noi = {};
+
+  if (type === 'data') name = 'data-' + name;
+
+  if (type === 'data' || type === 'attr') {
+    elems = dom.querySelectorAll('[' + name + ']');
+  } else if (type === 'class') {
+    elems = dom.querySelectorAll('[class*="' + name + '-"]');
+  } else {
+    throw new Error('type "' + type + '" unsupported');
+  }
+
+  Array.from(elems).forEach(function(e){
+
+    var letName;
+
+    if (type === 'data' || type === 'attr') {
+      letName = e.getAttribute(name);
+    } else {
+      letName = e.className.match(new RegExp(name + '-' + '((-?\\w+)+)'));
+      if (letName && letName.length) letName = letName[1];
+    }
+
+    if (!letName) return;
+    if (!dom.noi[letName]) dom.noi[letName] = [];
+    if (dom.noi[letName]) dom.noi[letName].push(e);
+
+  });
+
+  return dom;
+}
+
+function contextualFragmentToTemplate(fragment) {
+  var retval = fragment.children;
+  return objectAssign(retval, fragment.noi);
+}
+
+function process(source) {
+  return function(data, partials) {
+    return contextualFragmentToTemplate(getElementsFromDom(
+      Mustache.render(source, data, partials),
+      'attr',
+      'data-doomify'));
+  };
+}
+
+module.exports = process;
