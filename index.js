a6c2d1b9d24a720322f78d5547648c2357f04bb8
Tomasz Kociołek <tomasz.kociolek@k2.pl>
2016-02-09 13:00:23 +0100

    progress

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 var minify = require('html-minifier').minify;
 var Mustache = require('mustache');
+var stream = require('stream');
+var util = require('util');
+var objectAssign = require('object-assign');
+
+var MINIFIER_DEFAULTS = {
+  // http://perfectionkills.com/experimenting-with-html-minifier/#options
+  removeComments: true,
+  collapseWhitespace: true,
+  conservativeCollapse: true
+};
+
+var DEFAULTS = {
+  templateOpts: {},
+  minifierOpts: {},
+  noMinify: false
+};
 
 function compile(str, minifierOpts) {
   var minified = minifierOpts === false ? str : minify(str, minifierOpts);
@@ -8,7 +24,7 @@ function compile(str, minifierOpts) {
 
 function wrap(source) {
   return 'var doomify = require(\'doomify\');\n' +
-    'module.exports = doomify(' + source + ');';
+    'module.exports = doomify.template(\'' + source + '\');';
 }
 
 function transform(src, opts) {
@@ -88,11 +104,50 @@ function getElementsFromDom(dom, type, name) {
   return dom;
 }
 
-function doomify(source) {
-  var template = Mustache.parse(source);
+var templateExtension = /\.(dmt|tpl|html)$/;
+
+function Doomify(opts) {
+  stream.Transform.call(this);
+
+  opts = objectAssign({}, opts, DEFAULTS);
+
+  if (opts.minifierOpts !== false) {
+    opts.minifierOpts = objectAssign({}, opts.minifierOpts, MINIFIER_DEFAULTS);
+  }
+
+  this._data = '';
+  this._opts = opts;
+}
+
+util.inherits(Doomify, stream.Transform);
+
+Doomify.prototype._transform = function (buf, enc, next) {
+  this._data += buf;
+  next();
+};
+
+Doomify.prototype._flush = function (next) {
+  try {
+    this.push(transform(this._data, this._opts));
+  } catch(err) {
+    this.emit('error', err);
+    return;
+  }
+  next();
+};
+
+function doomify(file, opts) {
+  if (!templateExtension.test(file)) {
+    return new stream.PassThrough();
+  }
+  return new Doomify(opts);
+}
+
+function template(source) {
+  var cachedTemplate = Mustache.parse(source);
   return function(data, partials) {
     return getElementsFromDom(
-      Mustache.render(template, data, partials),
+      Mustache.render(cachedTemplate, data, partials),
       'attr',
       'data-doomify');
   };
@@ -102,3 +157,4 @@ module.exports = doomify;
 module.exports.compile = compile;
 module.exports.wrap = wrap;
 module.exports.transform = transform;
+module.exports.template = template;
