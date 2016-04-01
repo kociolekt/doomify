f9a39e8ce5afe7473215102ef25299b63092e1cd
Tomasz Kociołek <tomasz.kociolek@k2.pl>
2016-04-01 18:05:59 +0200

    docs

--- README.md
+++ README.md
@@ -1,3 +1,51 @@
 # doomify
 
-> Don't let DOM be your doom!
+> Auto-variablizing browserify plugin to require [Mustache](https://github.com/janl/mustache.js) template files.
+
+## installation
+Install from npm:
+```
+$ npm install doomify
+```
+
+## usage
+Use it as browserify transform module with -t:
+```
+$ browserify -t doomify main.js -o bundle.js
+```
+
+Or use it as browserify transform module in gulp:
+```js
+browserify({
+  entries: 'main.js',
+  debug: true,
+  transform: [
+    'doomify'
+  ]
+});
+```
+
+Mustache template files (.dmt, .tpl, .html)
+```html
+<!-- template.html -->
+<div class="example">
+  {{content}}
+  <div class="button" data-doomify="exampleButton"></div>
+</div>
+```
+
+Require inside main.js and invoke like Mustache template. Use it like DOM Element with cached nodes in object fields.
+```js
+// main.js
+var template = require('../../templates/template.html'); // Mustache template
+
+// Append template with javascript
+var templateDOM = template({content: 'Hello'}); // HTML DOM DocumentFragment
+document.body.appendChild(templateDOM);
+
+// Bind events
+var templateButton = templateDOM.exampleButton // Cached HTML DOM Element Object
+templateButton.addEventListener('click', function(){
+  alert(1);
+});
+```
--- process/index.js
+++ process/index.js
@@ -1,81 +1,14 @@
 var
   Mustache = require('mustache'),
-  objectAssign = require('object-assign');
+  autoElemsFromDom = require('auto-elems-from-dom');
 
-// ie 9, ie 10
-if (
-  (typeof Range !== 'undefined') &&
-  !Range.prototype.createContextualFragment
-) {
-  Range.prototype.createContextualFragment = function(html) {
-    var
-      frag = document.createDocumentFragment(),
-      div = document.createElement('div');
-    frag.appendChild(div);
-    div.outerHTML = html;
-    return frag;
-  };
-}
-
-// without additional div
-function getDocumentFragmentFromString(html) {
-  // return document.createRange().createContextualFragment(html); // FU safari
-  var range = document.createRange();
-  range.selectNode(document.body); // safari
-  return range.createContextualFragment(html);
-}
-
-function getElementsFromDom(dom, type, name) {
-  var
-    pairs = {},
-    elements;
-
-  if (!name) throw new Error('no-name not allowed');
-
-  if (typeof dom === 'string') {
-    dom = getDocumentFragmentFromString(dom);
-  }
-
-  // check also for getDocumentFragmentFromString
-  if (!dom.nodeType) {
-    throw new Error('string or DOM node');
-  }
-
-  if (type === 'data') name = 'data-' + name;
-
-  if (type === 'data' || type === 'attr') {
-    elements = dom.querySelectorAll('[' + name + ']');
-  } else if (type === 'class') {
-    elements = dom.querySelectorAll('[class*="' + name + '-"]');
-  } else {
-    throw new Error('type "' + type + '" unsupported');
-  }
-
-  for(var i = 0, eLen = elements.length; i < eLen; i++) {
-    var
-      element = elements[i],
-      key;
-
-    if (type === 'data' || type === 'attr') {
-      key = element.getAttribute(name);
-    } else {
-      key = element.className.match(new RegExp(name + '-' + '((-?\\w+)+)'));
-      if (key && key.length) key = key[1];
-    }
-
-    if (!key) continue;
-    if (!pairs[key]) pairs[key] = [];
-    if (pairs[key]) pairs[key].push(element);
-  }
-
-  return objectAssign(dom.children, pairs);
-}
+require('auto-elems-from-dom/polyfill');
 
 function process(source) {
   return function(data, partials) {
     var
       template = Mustache.render(source, data, partials),
-      fragment = getElementsFromDom(template, 'attr', 'data-doomify');
+      fragment = autoElemsFromDom(template, 'attr', 'data-doomify', true);
     return fragment;
   };
 }
